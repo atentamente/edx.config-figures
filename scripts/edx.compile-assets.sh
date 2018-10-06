0d8355846749108835498e9970f64ec31e2896dc
Lawrence McDaniel <lpm0073@gmail.com>
2018-10-06 14:07:56 -0500

    initialize banorte theme copy and scripts

---
+++ scripts/edx.compile-assets.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# update assets as edxapp user
+sudo -H -u edxapp bash << EOF
+source /edx/app/edxapp/edxapp_env
+cd /edx/app/edxapp/edx-platform
+paver update_assets lms --settings=aws
+paver update_assets cms --settings=aws
+EOF
+
+# restart edx instances
+sudo /edx/bin/supervisorctl restart edxapp:
+/edx/bin/supervisorctl restart edxapp_worker:
---
+++ scripts/edx.install-theme.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf edx.config-atentamente
+
+git clone https://github.com/lpm0073/edx.config-figures.git
+chown edxapp -R edx.config-figures
+chgrp edxapp -R edx.config-figures
+
+cp /home/ubuntu/edx.config-figures/conf/lms.env.json /edx/app/edxapp/lms.env.json
+cp /home/ubuntu/edx.config-figures/conf/lms.auth.json /edx/app/edxapp/lms.auth.json
+cp /home/ubuntu/edx.config-figures/conf/cms.env.json /edx/app/edxapp/cms.env.json
+cp /home/ubuntu/edx.config-figures/conf/cms.auth.json /edx/app/edxapp/cms.auth.json
+
+chmod 644 /edx/app/edxapp/*.env.json
+chmod 755 /edx/app/edxapp/*.auth.json
