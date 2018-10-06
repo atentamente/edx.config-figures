ddce5056ad8d180eb044ca3d4d03bb14178b3723
Lawrence McDaniel <lpm0073@gmail.com>
2018-10-06 15:11:10 -0500

    enable custom theming

--- scripts/edx.install-theme.sh
+++ scripts/edx.install-theme.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-rm -rf edx.config-atentamente
+rm -rf edx.config-figures
+rm -rf /edx/app/edxapp/edx-platform/themes/atentamente
 
 git clone https://github.com/atentamente/edx.config-figures.git
 chown edxapp -R edx.config-figures
-chgrp edxapp -R edx.config-figures
+chgrp www-data -R edx.config-figures
 
+# Install json config files
 cp /home/ubuntu/edx.config-figures/conf/lms.env.json /edx/app/edxapp/lms.env.json
 cp /home/ubuntu/edx.config-figures/conf/lms.auth.json /edx/app/edxapp/lms.auth.json
 cp /home/ubuntu/edx.config-figures/conf/cms.env.json /edx/app/edxapp/cms.env.json
@@ -13,3 +15,12 @@ cp /home/ubuntu/edx.config-figures/conf/cms.auth.json /edx/app/edxapp/cms.auth.j
 
 chmod 644 /edx/app/edxapp/*.env.json
 chmod 755 /edx/app/edxapp/*.auth.json
+
+# Install custom themes
+sudo cp -R /home/ubuntu/edx.config-figures/themes /edx/app/edxapp/edx-platform/
+sudo chown edxapp -R /edx/app/edxapp/edx-platform/themes
+sudo chgrp edxapp -R /edx/app/edxapp/edx-platform/themes
+
+#/edx/bin/supervisorctl restart lms
+#/edx/bin/supervisorctl restart cms
+#/edx/bin/supervisorctl restart edxapp_worker:
