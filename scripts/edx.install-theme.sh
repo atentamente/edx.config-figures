141459d9de25f49435c0bb3eca1b3b661af20c6c
Lawrence McDaniel <lpm0073@gmail.com>
2018-10-17 13:38:09 -0500

    modify installation for Figures

--- scripts/edx.install-theme.sh
+++ scripts/edx.install-theme.sh
@@ -21,6 +21,16 @@ sudo cp -R /home/ubuntu/edx.config-figures/themes /edx/app/edxapp/edx-platform/
 sudo chown edxapp -R /edx/app/edxapp/edx-platform/themes
 sudo chgrp edxapp -R /edx/app/edxapp/edx-platform/themes
 
-#/edx/bin/supervisorctl restart lms
-#/edx/bin/supervisorctl restart cms
-#/edx/bin/supervisorctl restart edxapp_worker:
+echo .............................
+echo Install Figures configuration
+echo .............................
+cp /home/ubuntu/edx.config-figures/lms/envs/aws.py /edx/app/edxapp/edx-platform/lms/envs/
+cp /home/ubuntu/edx.config-figures/lms/urls.py /edx/app/edxapp/edx-platform/lms/
+
+sudo chown -R edxapp /edx/app/edxapp/edx-platform/lms/
+sudo chgrp -R edxapp /edx/app/edxapp/edx-platform/lms/
+
+
+/edx/bin/supervisorctl restart lms
+/edx/bin/supervisorctl restart cms
+/edx/bin/supervisorctl restart edxapp_worker:
